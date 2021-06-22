0b905aa705faf4a8e45c6408305a4ff3b6c60649
Kyle Harris <Kylebradlyharris@gmail.com>
2021-06-21 20:31:16 -0500

    fixed print statement grammmar

--- 3/bmi2.0.py
+++ 3/bmi2.0.py
@@ -6,10 +6,10 @@ bmi = (weight/(height**2))
 if bmi <= 18.5:
     print(f"Your BMI is {bmi}, you are underweight.")
 elif 18.5 < bmi <= 25:
-    print(f"Your BMI is {bmi}. you are normal weight.")
+    print(f"Your BMI is {bmi}, you are normal weight.")
 elif 25 < bmi <= 30:
-    print(f"Your BMI is {bmi}. you are overweight.")
+    print(f"Your BMI is {bmi}, you are overweight.")
 elif 30 < bmi <= 35:
-    print(f"Your BMI is {bmi}. you are obese.")
+    print(f"Your BMI is {bmi}, you are obese.")
 elif bmi > 35:
-    print(f"Your BMI is {bmi}. you are clinically obese.")
+    print(f"Your BMI is {bmi}, you are clinically obese.")
