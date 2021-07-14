73c1108f0507a31380d28433eb7d71d3a9cc31b8
Kyle <Kylebradlyharris@gmail.com>
2021-07-13 22:30:36 -0500

    dict in list first attempt

---
+++ 9/dict_in_list.py
@@ -0,0 +1,23 @@
+travel_log = [
+    {
+        "country" : "France",
+        "visits" : 12,
+        "cities" : ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country" : "Germany",
+        "visits" : 5,
+        "cities" : ["Berlin", "Hamburg", "Stuttgart"]
+    }
+]
+
+def add_new_country(country, visit_num, city_list):
+    new_country = {}
+    new_country["country"] = country
+    new_country["visits"] = visit_num
+    new_country["cities"] = city_list
+    travel_log.append(new_country)
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+
+print(travel_log)
