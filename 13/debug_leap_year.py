6b458b70d554f6302b688805c61bdb1d59b1ccd6
Kyle Harris <Kylebradlyharris@gmail.com>
2021-07-21 05:47:27 -0500

    bugged

---
+++ 13/debug_leap_year.py
@@ -0,0 +1,12 @@
+year = input("Which year do your want to check? :")
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not Leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not Leap year")
