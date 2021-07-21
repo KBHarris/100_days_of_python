9a484d43a7be624b6203de3150f19ca86abd1f21
Kyle <Kylebradlyharris@gmail.com>
2021-07-21 05:36:53 -0500

    Bug corrected

--- 13/debug_odd_odd_or_even.py
+++ 13/debug_odd_odd_or_even.py
@@ -1,6 +1,7 @@
 number = int(input("Which number do you wnat to check?: "))
 
-if number % 2 = 0:
+#Bug was caused by the if statement using an assignment operator instead of an equivalency operator
+if number % 2 == 0:
     print("This number is even.")
 else:
     print("This number is odd.")
